ddb4ce0dbcb54993bdc4df3953c0078de3d40874
Divyansh-Pratap <dpthegreat224@gmail.com>
2023-01-05 21:10:00 +0530

    renaming done

--- Print_adjacency_list.java
+++ Print_adjacency_list.java
@@ -2,13 +2,13 @@ class Solution {
 
     public ArrayList<ArrayList<Integer>> printGraph(int V, ArrayList<ArrayList<Integer>> adj) {
 
-            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
             for(int i = 0 ; i<V ; i++){
-                graph.add(new ArrayList<>());
+                adj.add(new ArrayList<>());
                 for(int v : adj.get(i)){
-                    graph.get(i).add(v);
+                    adj.get(i).add(v);
                 }
             }
-            return graph;
+            return adj;
     }
 }
